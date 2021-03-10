f66bfc33174f4904b497041c1b1c856a8e924e97
sonyoon7 <sonyoon7@gmail.com>
2021-03-10 15:44:39 +0900

    node test 커밋

---
+++ routes/index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+
+
+  console.log(process.env.env)
+  console.log(req.app.get('env'))
+  console.log('ssss')
+  res.send(env);
+});
+
+module.exports = router;
